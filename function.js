9d32c7f0d51371699798a1f2c6175ca5809317ae
Ayani1026 <89828615+Ayani1026@users.noreply.github.com>
2022-02-24 20:35:35 +0700

    js

---
+++ function.js
@@ -0,0 +1,26 @@
+function satuFungsi(){
+    alert("Halo!");
+    console.log("Salam kenal");
+}
+
+function duaFungsi(){
+    alert("Nama Saya Ayanis");
+    console.log("Saya manusia");
+}
+
+function tigaFungsi(){
+    alert("Tugas Eduwork");
+    console.log("Javascript Funsction");
+}
+
+function rumusPersegi(){
+    let x = persegi(4, 3);
+    function persegi(a, b){
+        alert("Rumus Persegi");
+        console.log("Rumus = sisi * sisi");
+        c = a*b;
+        console.log("a = ",a);
+        console.log("a = ",b);
+        console.log("hasil",c);
+    }
+}
